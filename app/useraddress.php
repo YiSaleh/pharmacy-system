a0d5f4695b7b3df1c71f519ed31d74057328c9e3
marina00matta <marinamedhat_19@hotmail.com>
2020-03-30 22:45:08 +0200

    relations between tables in database

--- app/useraddress.php
+++ app/useraddress.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class useraddress extends Model
 {
-    //
+    
+    public function pharmacyusers()
+    {
+        return $this->belongsTo('App\pharmacyusers');
+    }
 }
