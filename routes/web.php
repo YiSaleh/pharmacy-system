46a84267f6f949f736b20fa3409a0df8986a864e
Mahmoud-ramadan727 <you@example.com>
2020-03-30 15:18:49 +0200

    remove register feature from App

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
- 
+Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
