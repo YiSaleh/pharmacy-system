7048285ed8f8df5edbf69a2a3d05a1cf1324d56b
Mahmoud-ramadan727 <you@example.com>
2020-04-01 21:07:02 +0200

    greeting verified user by sending email

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\Events\Verified;
+use App\Notifications\UserVerified;
 
 class VerificationController extends Controller
+
 {
     /*
     |--------------------------------------------------------------------------
@@ -35,8 +41,21 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
+    public function verify(Request $request)
+{
+    $user = User::find($request->route('id'));
+
+    if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+        throw new AuthorizationException;
+    }
+
+    if ($user->markEmailAsVerified())
+        $user->notify(new UserVerified());
+
+    return "Please check your mail";
+}
+
 }
