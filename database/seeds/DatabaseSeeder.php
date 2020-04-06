7638d23c0553b1ebfe57047deb0df4d85a1fb608
nada <n.a.hassan1994@gmail.com>
2020-04-06 13:07:51 +0200

    make admin account by laravel seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // DB::table('users')->insert([
-        //     'name' => 'admin',
-        //     'email' => 'admin@admin.com',
-        //     'password' => Hash::make('123456'),
-        //     'phone'=>01001001001,
-
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+            'phone'=>01001001001,
+            'gender'=>'Male',
+            'date_of_birth'=>'1990/12/01',
+            'national_id'=>23456789022113 ,
+        ]);
     }
 }
