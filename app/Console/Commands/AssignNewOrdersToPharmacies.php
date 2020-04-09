68943ca1805b393f1e7b092dc379b92603489043
Mahmoud-ramadan727 <you@example.com>
2020-04-09 23:01:34 +0200

    schedule command that runs every minute​ to scan the new ​ orders and assign them to the highest priority pharmacy

---
+++ app/Console/Commands/AssignNewOrdersToPharmacies.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Order;
+use App\User_Address;
+use App\Pharmacy;
+
+class AssignNewOrdersToPharmacies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'assign:new-orders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assign new orders to the highest priority pharmacy';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+       $new_orders=Order::where('status','new')->get(); 
+       foreach ($new_orders as $new_order ) {
+           $user_address=User_Address::where('id',$new_order->user_address_id)->first();
+           $pharmacy=Pharmacy::where('area_id',$user_address->area_id)->where('periority','High')->first();
+           $new_order->pharmacy_id=$pharmacy->id;
+           $new_order->save();
+       } 
+    }
+}
